ea208942b766f93cca5d41150631df3e4938365e
IhorOvchinnikov <beliy811@gmail.com>
2022-08-15 11:15:52 +0300

    first_task

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,9 +1,10 @@
 from pages.base_page import BasePage
 
 class Dashboard(BasePage):
+
 button_xpath = "//*@id='login']"
 home_page_xpath "//*[text()='Main page']"
 page_Players_xpath "//*[text()='Players']"
 language_switching_xpath "//*[@id="__next"]/div[1]/div/div/div/ul[2]/div[1]/div[2]/span"
-home_page_xpath "//*[text()='Main page']"
 futbol_kolektyw_logo_xpath = "//*[@title="Logo Scouts Panel"]"
+add_player_button_xpath = "//*[text()='Add player']"
